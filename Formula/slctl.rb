5d3f4f317b26aa9a19290aa37629d12b3670e89e
softleader/homebrew-tap/brew-tapper <supprt@softleader.com.tw>
2026-01-18 19:05:17 +0800

    version upgrade by brew-tapper bot

--- Formula/slctl.rb
+++ Formula/slctl.rb
@@ -1,23 +1,23 @@
 class Slctl < Formula
   desc "Slctl is a command line interface for running commands against SoftLeader Services"
   homepage "https://github.com/softleader/slctl"
-  version "3.8.1"
+  version "4.0.0"
   
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-darwin-arm64-#{version}.tgz"
-      sha256 "0c9b99f2bf3d526ae7197f37675e6ed1fe059c29ee93f2ca51b4fb41503fb5fd"
+      sha256 ""
     else
       url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-darwin-amd64-#{version}.tgz"
-      sha256 "0c9b99f2bf3d526ae7197f37675e6ed1fe059c29ee93f2ca51b4fb41503fb5fd"
+      sha256 ""
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-linux-arm64-#{version}.tgz"
-      sha256 "5eb622c25a37c1a7a373f4036f51a7363663032e4436f2e33bb92cd7febb4628"
+      sha256 ""
     else
       url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-linux-amd64-#{version}.tgz"
-      sha256 "5eb622c25a37c1a7a373f4036f51a7363663032e4436f2e33bb92cd7febb4628"
+      sha256 ""
     end
   end
 
